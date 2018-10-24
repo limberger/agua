399f9b58d8b8f1b8eec1ab74bbd685222f6b2c21
Joao Paulo Limberger - SHOO <jlimberger@gmail.com>
2018-10-23 23:25:38 -0300

    initiall

--- agua/admin.py
+++ agua/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from agua.models import Agua
+from agua.models import Condominio, Condomino, CondominoDoCondominio, Hidrometro, Medicao, TipoDespesa, Despesa
 
 # Register your models here.
 
-admin.site.register(Agua)
+admin.site.register(Condominio)
+admin.site.register(Condomino)
+admin.site.register(CondominoDoCondominio)
+admin.site.register(Hidrometro)
+admin.site.register(Medicao)
+admin.site.register(TipoDespesa)
+admin.site.register(Despesa)
